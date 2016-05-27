6bd299df7b8c2dbc0eb76b5ffcb509337c4990b7
JB Osborne <jozbone@live.com>
2016-05-27 16:48:47 -0400

    adding basic projects

---
+++ eManager/eManager.Web/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace eManager.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "You hit the index action of the home controller";
+
+            // tells MVC runtime to render a "conventional view"
+            return View();
+            // you can also explicitly specify it like this:
+            // return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
