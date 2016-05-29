5b662681404ef6651a09df42515d55c53de2d402
JB Osborne <jozbone@live.com>
2016-05-29 12:43:25 -0400

    trivia

--- AsyncAwaitSamples/AsyncAwaitSamples/SomnezInteviewQuestionscs.cs
+++ AsyncAwaitSamples/AsyncAwaitSamples/SomnezInteviewQuestionscs.cs
@@ -2,39 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AsyncAwaitSamples
 {
     [TestClass]
-    public class DelaySamples
+    public class SomnezSampleInterviewQuestions
     {
-        [TestMethod]
-        public void Demonstrates_a_delay()
-        {
-            var task = SlowOperationAsync();
-            
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(i);
-            }
-
-            Console.WriteLine("Slow operation result is: {0}", task.Result);
-            Console.WriteLine("Slow operation test completed on {0}", Thread.CurrentThread.ManagedThreadId);
-        }
-
-        private static async Task<int> SlowOperationAsync()
-        {
-            Console.WriteLine("Slow operation started on {0}", Thread.CurrentThread.ManagedThreadId);
-
-            await Task.Delay(2000);
-
-            Console.WriteLine("Slow operation completed on {0}", Thread.CurrentThread.ManagedThreadId);
-
-            return 42;
-        }
 
         [TestMethod]
         public void SomnezSampleInterviewQuestions_Score_XRayMachine()
@@ -57,33 +32,33 @@ namespace AsyncAwaitSamples
         }
 
 
-        class Scorer
+        private class Scorer
         {
             private Dictionary<char, int> scoreValues = new Dictionary<char, int>()
-            {
-                {'f', 3},
-                {'j', 6},
-                {'x', 12},
-                {'a', 2},
-                {'e', 2},
-                {'i', 2},
-                {'o', 2},
-                {'t', 5},
-            };
+                {
+                    {'f', 3},
+                    {'j', 6},
+                    {'x', 12},
+                    {'a', 2},
+                    {'e', 2},
+                    {'i', 2},
+                    {'o', 2},
+                    {'t', 5},
+                };
 
             public int CalculateScore(string input)
             {
                 var sum = 0;
 
-                foreach(var item in input.ToLower().ToCharArray())
+                foreach (var item in input.ToLower().ToCharArray())
                 {
                     sum += ScoreCharacter(item);
                 }
 
                 return sum;
-            }   
-            
-               
+            }
+
+
             private int ScoreCharacter(char c)
             {
                 int characterValue = 0;
@@ -96,5 +71,6 @@ namespace AsyncAwaitSamples
                 return characterValue;
             }
         }
-    }    
+    }
+
 }
