d4ae9c9a0c81ecf6df51ad2b47242937dce903f4
JB Osborne <jozbone@live.com>
2016-05-28 22:05:11 -0400

    messing arround

--- eManager/eManager.Web/Controllers/HomeController.cs
+++ eManager/eManager.Web/Controllers/HomeController.cs
@@ -1,4 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 using eManager.Domain;
 
 namespace eManager.Web.Controllers
@@ -12,14 +19,45 @@ namespace eManager.Web.Controllers
             _db = db;
         }
 
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            var allDepartments = _db.Departments;
+            var sw = new Stopwatch();
+            sw.Start();
+            
+            var allDepartments = _db.Departments.ToListAsync();
+            var number = SlowOperationAsync();
+            await Task.WhenAll(allDepartments, number);
 
-            // tells MVC runtime to render a "conventional view"
-            return View(allDepartments);
+            sw.Stop();
+
+            var timeTaken = sw.ElapsedMilliseconds;
+            return View(allDepartments.Result);
             // you can also explicitly specify it like this:
             // return View("Index");
+        }
+
+        private async Task<IEnumerable<Department>> GetAllDepartments()
+        {
+            //var task = SlowOperationAsync();
+            //            Console.WriteLine("Slow operation result is: {0}", task.Result);
+            //            Console.WriteLine("Slow operation test completed on {0}", Thread.CurrentThread.ManagedThreadId);
+
+            var departmentTask = await _db.Departments.ToListAsync();
+//            task.Wait();
+            return departmentTask;
+
+            // tells MVC runtime to render a "conventional view"
+        }
+
+        private static async Task<int> SlowOperationAsync()
+        {
+            //Console.WriteLine("Slow operation started on {0}", Thread.CurrentThread.ManagedThreadId);
+
+            await Task.Delay(2000);
+
+            //Console.WriteLine("Slow operation completed on {0}", Thread.CurrentThread.ManagedThreadId);
+
+            return 42;
         }
 
         public ActionResult About()
